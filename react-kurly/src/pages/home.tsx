8e5b01a0b9d2136838cb846087b37d315f96385f
alreadynyeong <kjw87130@naver.com>
2023-04-06 19:10:56 +0900

    홈페이지 및 컴포넌트 파일 생성

---
+++ react-kurly/src/pages/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Top from '../components/top'
+import Category from '../components/category'
+
+export default function Home() {
+  return (
+    <>
+    <Top/>
+    <Category/>
+    </>
+  )
+}
