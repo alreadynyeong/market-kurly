1745b8ca9181aad22683cff61dbeab25957af1c7
alreadynyeong <kjw87130@naver.com>
2023-04-07 15:07:53 +0900

    [feat] 로그인, 회원가입 페이지 연결

--- react-kurly/src/App.tsx
+++ react-kurly/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
       </Routes>
     </BrowserRouter>
   );
