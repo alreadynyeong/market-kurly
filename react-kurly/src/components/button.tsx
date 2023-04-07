efe33aaa8376390231646843c329c4a99f12b819
alreadynyeong <kjw87130@naver.com>
2023-04-07 15:37:48 +0900

    [feat] 버튼 컴포넌트 추가

---
+++ react-kurly/src/components/button.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledPurpleButton = styled.div`
+    text-align: center;
+    display: table-cell;
+    vertical-align: middle;
+    height: 50px;
+    font-size: large;
+    border-radius: 3px;
+    margin-top: 20px;
+    border: 1px solid purple;
+    background-color: purple;
+    color: white;
+}
+`
+const StyledPurpleLineButton = styled.div`
+    text-align: center;
+    display: table-cell;
+    vertical-align: middle;
+    height: 50px;
+    font-size: large;
+    border-radius: 3px;
+    margin-top: 20px;
+    border: 1px solid purple;
+    background-color: white;
+    color: purple;
+}
+`
+interface ButtonProps {
+    text?: string;
+    on?: string;
+    wid?: number;
+}
+
+export const PurpleButton = ({
+    text,
+    on,
+    wid,
+    ...props
+  }: ButtonProps) => {
+    console.log(wid)
+    return (
+      <StyledPurpleButton
+      style={{width:`${wid}px` }}>
+        {text}
+      </StyledPurpleButton>
+    );
+};
+  export const PurpleLineButton = ({
+    text,
+    on,
+    wid,
+    ...props
+  }: ButtonProps) => {
+    console.log(wid)
+    return (
+      <StyledPurpleLineButton
+      style={{width:`${wid}px` }}>
+        {text}
+      </StyledPurpleLineButton>
+    );
+};
+  
