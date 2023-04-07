3222b21a0824b87a5ba1f5392179d4f570386cfc
alreadynyeong <kjw87130@naver.com>
2023-04-07 14:41:52 +0900

    [feat] 배너 추가

--- react-kurly/src/pages/home.tsx
+++ react-kurly/src/pages/home.tsx
@@ -1,20 +1,38 @@
 import React from 'react'
 import styled from 'styled-components'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import Top from '../components/top'
 import Category from '../components/category'
 import Products from '../components/product'
 
 const Banner = styled.div`
-  background-color: cadetblue;
     height: 379px;
 `
+
 export default function Home() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+  
   return (
     <>
     <Top/>
     <Category/>
-    <Banner/>
+    <Banner>
+      <Slider {...settings}>
+      <img src="https://placeimg.com/600/200/animals" />
+      <img src="https://placeimg.com/600/200/animals" />
+      <img src="https://placeimg.com/600/200/animals" />
+      <img src="https://placeimg.com/600/200/people" />
+      </Slider>
+    </Banner>
     <Products/>
     
     </>
